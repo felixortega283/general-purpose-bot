ec3833393e949d079c5e944b1a7cae0b9cbd1ef7
Felix Ortega <ortegafelix226@icloud.com>
2026-06-01 13:15:56 -0500

    Finished setting up typescript and added command files

--- src/commands/utility/ping.ts
+++ src/commands/utility/ping.ts
@@ -1,4 +1,5 @@
 import { SlashCommandBuilder, CommandInteraction } from "discord.js";
+
 module.exports = {
 	data: new SlashCommandBuilder().setName('ping').setDescription('Replies with Pong!'),
 	async execute(interaction: CommandInteraction) {
---
+++ src/commands/utility/server.ts
@@ -0,0 +1,11 @@
+import { SlashCommandBuilder, CommandInteraction, ChatInputCommandInteraction } from "discord.js";
+
+module.exports = {
+	data: new SlashCommandBuilder().setName('server').setDescription('Provides information about the server.'),
+	async execute(interaction: { reply: (arg0: string) => any; guild: { name: string; memberCount: number; }; }) {
+		// interaction.guild is the object representing the Guild in which the command was run
+		await interaction.reply(
+			`This server is ${interaction.guild.name} and has ${interaction.guild.memberCount} members.`,
+		);
+	},
+};
